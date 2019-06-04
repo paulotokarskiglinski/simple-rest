a0ca79512f0996c83e6c0266f109bbfd69e6a8bb
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2019-06-03 22:52:49 -0300

    Commit

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const router = {
 	'ping': handlers.ping
 };
 
+const httpServer = http.createServer(function(req, res) {
+	server(req, res);
+}).listen(process.env.PORT || 5000);
+
 httpsServerOptions = {
 	'key': fs.readFileSync('./https/key.pem'),
 	'cert': fs.readFileSync('./https/cert.pem')
@@ -27,17 +31,15 @@ const httpsServer = https.createServer(httpsServerOptions, function(req, res) {
 }).listen(process.env.PORT || 6000);
 
 const server = function(req, res) {
-    req.on('end', function() {
-        var parsedUrl = url.parse(req.url, true);
-        var method = req.method.toLowerCase();
-        var path = parsedUrl.pathname;
-        var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-        
-        var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
-        chosenHandler(function(statusCode, payload) {
-            res.writeHead(statusCode, {'Content-Type': 'application/json'});
-            res.write(JSON.stringify(payload));
-            res.end();
-        });
+    var parsedUrl = url.parse(req.url, true);
+    var method = req.method.toLowerCase();
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+    
+    var chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    chosenHandler(function(statusCode, payload) {
+        res.writeHead(statusCode, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify(payload));
+        res.end();
     });
 }
