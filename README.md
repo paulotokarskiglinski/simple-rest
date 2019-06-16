23efcb2bb2e53fcea20f3cc331e93d75961c3ebf
Paulo Henrique Tokarski Glinski <paulotokarski@gmail.com>
2019-06-16 17:50:59 -0300

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,13 @@
+# Simple REST
+
+Esta é uma API REST desenvolvida em NodeJS que não utiliza nenhuma dependência de pacote para o projeto. Esse modelo base pode ser utilizado como base para o desenvolvimento de APIs mais sofisticadas.
+
+**Versão online:** https://simple-rest-nodejs.herokuapp.com/
+
+## Rotas
+
+**/** -> Status da API <br>
+**/usuarios** -> Retorna uma lista dos usuários <br>
+**/usuarios?id=0** -> Retorna uma lista de usuários filtrados pelo parâmetro passado <br>
+**/posts** -> Retorna uma lista de posts <br>
+**/posts?id=0** -> Retorna uma lista de posts filtrados pelo parâmetro passado <br>
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ handlers.usuarios = function(data, callback) {
       if(['get'].indexOf(data.method) > -1){
         handlers._usuarios[data.method](usuarios, data, callback);
       } else {
-        callback(405, {error: 405, message: "Método não permitido"});
+        callback(405, {error: 405});
       }
     }
   });
@@ -24,12 +24,18 @@ handlers.usuarios = function(data, callback) {
 
 handlers._usuarios.get = function(usuarios, data, callback) {
   const array = JSON.parse(usuarios);
-  const id = typeof(data.queryParams.id) == 'string' ? data.queryParams.id : false;
+  const parametros = typeof(data.queryParams) == 'object' && Object.keys(data.queryParams).length > 0 ? data.queryParams : false;
 
-  if (id) {
-    callback(202, array.filter(function(usuario) {
-      return usuario.id.toString() === id;
-    }));    
+  if (parametros) {
+    const resultados = [];
+    for (data in parametros) {
+      array.filter(function(usuario) {
+        if (usuario[data].toString().toLowerCase() === parametros[data].toString().toLowerCase()) {
+          resultados.push(usuario);
+        }
+      });
+    }
+    callback(202, resultados);    
   } else {
     callback(202, array);
   }
@@ -44,7 +50,7 @@ handlers.posts = function(data, callback) {
       if(['get', 'post', 'put', 'delete'].indexOf(data.method) > -1){
         handlers._posts[data.method](posts, data, callback);
       } else {
-        callback(405, {error: 405, message: "Método não permitido"});
+        callback(405, {error: 405});
       }
     }
   });
@@ -52,12 +58,18 @@ handlers.posts = function(data, callback) {
 
 handlers._posts.get = function(posts, data, callback) {
   const array = JSON.parse(posts);
-  const id = typeof(data.queryParams.id) == 'string' ? data.queryParams.id : false;
+  const parametros = typeof(data.queryParams) == 'object' && Object.keys(data.queryParams).length > 0 ? data.queryParams : false;
 
-  if (id) {
-    callback(202, array.filter(function(post) {
-      return post.id.toString() === id;
-    }));
+  if (parametros) {
+    const resultados = [];
+    for (data in parametros) {
+      array.filter(function(posts) {
+        if (posts[data].toString().toLowerCase() === parametros[data].toString().toLowerCase()) {
+          resultados.push(posts);
+        }
+      });
+    }
+    callback(202, resultados);
   } else {
     callback(202, array);
   }
@@ -72,7 +84,7 @@ handlers._posts.post = function(post, data, callback) {
     body: decodeURIComponent(item.split('&')[2].replace('body', '').replace('=', ''))
   };
 
-  array.push(novoItem)
+  array.push(novoItem);
   callback(202, array);
 };
 
@@ -91,6 +103,7 @@ const httpServer = http.createServer(function(req, res) {
 }).listen(process.env.PORT || 5000);
 
 const server = function(req, res) {
+  let body = [];
   const parsedUrl = url.parse(req.url, true);
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
@@ -98,7 +111,6 @@ const server = function(req, res) {
     queryParams: parsedUrl.query,
     method: req.method.toLowerCase()
   };
-  let body = [];
 
   req.on('data', function(conteudo) {
     body.push(conteudo);
